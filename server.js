9e0a98f0299920e3e9c5847ca23f0832845993ea
ak.yaps <contact@ayaba-kumako.com>
2019-06-16 22:39:12 +0200

    change scopes

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ let querystring = require('querystring')
 
 let app = express()
 
-let redirect_uri = 
-  process.env.REDIRECT_URI || 
-  'https://cryptic-caverns-89709.herokuapp.com/'
+let redirect_uri =
+  process.env.REDIRECT_URI ||
+  'http://localhost:8888/callback'
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      scope: 'user-read-private user-read-email user-read-recently-played playlist-read-collaborative playlist-read-private',
       redirect_uri
     }))
 })
 
-app.get('/callback', function(req, res) {
+app.get('/callback', function (req, res) {
   let code = req.query.code || null
   let authOptions = {
     url: 'https://accounts.spotify.com/api/token',
@@ -34,9 +34,9 @@ app.get('/callback', function(req, res) {
     },
     json: true
   }
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     var access_token = body.access_token
-    let uri = process.env.FRONTEND_URI || 'https://cryptic-caverns-89709.herokuapp.com/login'
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
     res.redirect(uri + '?access_token=' + access_token)
   })
 })
